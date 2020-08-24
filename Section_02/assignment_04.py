a779506bd825c87af8aa7bf4454688cf0ddc98f0
kumarankrypto <69230992+kumarankrypto@users.noreply.github.com>
2020-08-24 03:12:38 -0400

    Update assignment_04.py

--- Section_02/assignment_04.py
+++ Section_02/assignment_04.py
@@ -7,8 +7,8 @@ and replace the word TV with the word television. Then print my_list.
 my_list = [(1, 2), (3, 4), (['c', 'd', 'a', 'm'], [3, 9, 4, 12], 4), 'TV', 42]
 
 
-my_list[2][0][3] = 'x'
-my_list[3] = 'Television'
+my_list[2][0][3] = "x"
+my_list[3] = "Television"
 
 print(my_list)
 
