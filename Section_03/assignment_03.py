4724f41def8413424956b7ecf33145a46f4b3624
kumarankrypto <69230992+kumarankrypto@users.noreply.github.com>
2020-08-31 05:58:41 -0400

    Update assignment_03.py

--- Section_03/assignment_03.py
+++ Section_03/assignment_03.py
@@ -10,8 +10,8 @@ argument as individual elements in the list.
 
 """
 # Your Code Below:
-def multi_merge(list_a, str):
-    return list_a + str.split() + list(str)
+def multi_merge(list1, str):
+    return list1 + str.split() + list(str)
 
 print(multi_merge([1,2,3,4], "Hello My name is imtiaz"))
 
